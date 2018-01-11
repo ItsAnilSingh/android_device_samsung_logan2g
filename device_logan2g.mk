929eeb22d620476b68f3581b21f6d970b9f1e22e
ItsAnilSingh <canilsingh07@gmail.com>
2018-01-11 20:26:04 +0530

    Add: Trebuchet Workaround

--- device_logan2g.mk
+++ device_logan2g.mk
@@ -135,7 +135,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Languages
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.locale.language=en \
-    ro.product.locale.region=GB
+    ro.product.locale.region=US
 
 # SPRD default build.prop properties overrides
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -193,7 +193,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, $(LOCAL_PATH)/prebuilt/Android.mk)
 
 # Workaround for launcher
-$(call inherit-product-if-exists, device/samsung/workaround/launcher.mk)
+$(call inherit-product, device/samsung/logan2g/workaround/launcher.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_logan2g
